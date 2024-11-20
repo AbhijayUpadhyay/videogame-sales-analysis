587e5a717f53ffd3a848bcc715ee0036ed16d887
lukerepublic <117252639+lukerepublic@users.noreply.github.com>
2024-11-20 17:32:22 -0500

    Update sql_analysis.py

--- sql_analysis.py
+++ sql_analysis.py
@@ -1,9 +1,21 @@
+import os
 import sqlite3
 
+db_file_path = './videogames.db'
+
 # Connect to the database
-db = './videogames.db'
-conn = sqlite3.connect(db)
+def connect_to_db(db):
+
+    if not os.path.exists(db):
+        print(f"Database '{db}' does not exist.")
+        return None
+        
+    return sqlite3.connect(db)
+
+conn = connect_to_db(db_file_path)
 cursor = conn.cursor()
+
+## ======================================================================== ##
 
 # Find the top 10 selling games
 top_ten = cursor.execute('''
@@ -13,9 +25,27 @@ top_ten = cursor.execute('''
     FROM 
         sales
     ORDER BY 
-        sales_number
+        sales_number DESC
     LIMIT 10;
 ''')
 for row in top_ten:
     print(row)
 
+# Average sales for each genre
+avg_sales = cursor.execute('''
+    SELECT 
+        m.genres, 
+        AVG(s.sales_number) AS avg_sales
+    FROM 
+        sales s
+    JOIN 
+        metadata m
+    ON 
+        s.title = m.title
+    GROUP BY 
+        m.genres
+    ORDER BY 
+        avg_sales DESC;               
+''')
+for row in avg_sales:
+    print(row)
