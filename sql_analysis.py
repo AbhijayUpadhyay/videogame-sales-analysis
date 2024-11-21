e8d8557097e761142222897ed937b467fbc7382f
lukerepublic <117252639+lukerepublic@users.noreply.github.com>
2024-11-20 22:54:08 -0500

    Update sql_analysis.py

--- sql_analysis.py
+++ sql_analysis.py
@@ -1,51 +1,62 @@
 import os
 import sqlite3
 
-db_file_path = './videogames.db'
+# Average sales for each genre
+def avg_sales_genre(cursor):
 
-# Connect to the database
-def connect_to_db(db):
-
-    if not os.path.exists(db):
-        print(f"Database '{db}' does not exist.")
-        return None
-        
-    return sqlite3.connect(db)
-
-conn = connect_to_db(db_file_path)
-cursor = conn.cursor()
-
-## ======================================================================== ##
+    avg_sales = cursor.execute('''
+        SELECT 
+            m.genres, 
+            AVG(s.sales_number) AS avg_sales
+        FROM 
+            sales s
+        JOIN 
+            metadata m
+        ON 
+            s.title = m.title
+        GROUP BY 
+            m.genres
+        ORDER BY 
+            avg_sales DESC;               
+    ''')
+    for row in avg_sales:
+        print(row)
 
 # Find the top 10 selling games
-top_ten = cursor.execute('''
-    SELECT 
-        title, 
-        sales_number 
-    FROM 
-        sales
-    ORDER BY 
-        sales_number DESC
-    LIMIT 10;
-''')
-for row in top_ten:
-    print(row)
+def top_ten(cursor):
 
-# Average sales for each genre
-avg_sales = cursor.execute('''
-    SELECT 
-        m.genres, 
-        AVG(s.sales_number) AS avg_sales
-    FROM 
-        sales s
-    JOIN 
-        metadata m
-    ON 
-        s.title = m.title
-    GROUP BY 
-        m.genres
-    ORDER BY 
-        avg_sales DESC;               
-''')
-for row in avg_sales:
-    print(row)
+    top_ten = cursor.execute('''
+        SELECT 
+            title, 
+            sales_number 
+        FROM 
+            sales
+        ORDER BY 
+            sales_number DESC
+        LIMIT 10;
+    ''')
+    for row in top_ten:
+        print(row)
+
+
+def run_sql_queries(db):
+
+    # Connect to the database
+    def connect_to_db(db):
+
+        if not os.path.exists(db):
+            print(f"Database '{db}' does not exist.")
+            return None
+            
+        return sqlite3.connect(db)
+
+    conn = connect_to_db(db)
+    cursor = conn.cursor()
+
+    ## ======================================================================== ##
+
+    # Find the top 10 selling games
+    top_ten(cursor)
+
+    # Average sales for each genre
+    avg_sales_genre(cursor)
