2c758c849f002949e4265a31268f7d1083b7bfd5
lukerepublic <117252639+lukerepublic@users.noreply.github.com>
2024-11-20 22:53:45 -0500

    Update create_db.py

--- create_db.py
+++ create_db.py
@@ -1,40 +1,14 @@
-import pandas as pd
-import os
 import sqlite3
-import clean
 
-sales = './LargestCleanedDS.csv'
-metadata = './games.csv'
+def create_database(df_sales, df_metadata):
 
-## ======================================================================== ##
+    # Connect to the database
+    db = 'videogames.db'
+    conn = sqlite3.connect(db)
 
-# Convert csv files to DataFrames
-def read_csv_file(file_path):
+    # Add tables to the database
+    df_sales.to_sql('sales', conn, if_exists='replace', index=False)
+    df_metadata.to_sql('metadata', conn, if_exists='replace', index=False)
 
-    if not os.path.exists(file_path):
-        print(f"File '{file_path}' does not exist.")
-        return None
-    
-    df = pd.read_csv(file_path)
-    return df
-
-df_sales = read_csv_file(sales)
-df_metadata = read_csv_file(metadata)
-
-## ======================================================================== ##
-
-# Clean and format the dataframes
-df_sales = clean.process_dataframe(df_sales)
-df_metadata = clean.process_dataframe(df_metadata)
-
-## ======================================================================== ##
-
-# Connect to the database
-db = 'videogames.db'
-conn = sqlite3.connect(db)
-
-# Add tables to the database
-df_sales.to_sql('sales', conn, if_exists='replace', index=False)
-df_metadata.to_sql('metadata', conn, if_exists='replace', index=False)
-
-conn.close()
+    conn.close()
+    return db
